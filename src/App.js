5468db3115695e0ae9e37172cbb1321cd9798641
Bryan Miller <bmilcs@yahoo.com>
2022-12-28 15:03:50 -0500

    Add Game component & move pseudo-code to it

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
 import Header from "./components/Header";
+import Game from "./components/Game";
 import "./styles/App.scss";
 
 function App() {
-  // introduction page
-  //  explain game
-  //  start button
-
-  // create a master array, containg all card objects
-  //  { name: "HTML", icon: HTMLSVG.js }
-  //  pass array as prop to cards component
-  //  render with dataset attribute 'cardname'
-  //  pass eventhandler function
-
-  // state: initialize with:
-  //  1. array 1: all cards, random order
-  //  2. array 2: history of selected cards [empty]
-  //  3. currentScore
-  //  4. highScore
-
-  // event handler function for all cards
-  //  - on click, capture 'cardname' attr & check history array via .some()
-  //    - if no previous hits, randomize card order & increment score
-  //    - if card has been selected already:
-  //      - compare score to high score. replace if higher.
-  //      - reset current score to zero
-  //      - reset history array
-  //  - restart game loop: randomize cards, random order
-
-  // utility function:
-  //  randomize array order: override allCards array in state
-
-  // components
-  // app: primary component - state, handlers, logic
-  //   header:       page title / github link
-  //   scoreboard:   current / high score
-  //   gameboard:    grid container for all cards
-  //     card:       generate individual cards
-
   return (
     <>
       <Header />
+      <Game />
     </>
   );
 }
---
+++ src/components/Game.js
@@ -0,0 +1,42 @@
+import { cardList } from "../modules/cardList";
+import { useState } from "react";
+
+export default function Game() {
+  const [cards, setCards] = useState(cardList);
+
+  // create a master array, containg all card objects
+  //  { name: "HTML", icon: HTMLSVG.js }
+  //  pass array as prop to cards component
+  //  render with dataset attribute 'cardname'
+  //  pass eventhandler function
+
+  // state: initialize with:
+  //  1. array 1: all cards, random order
+  //  2. array 2: history of selected cards [empty]
+  //  3. currentScore
+  //  4. highScore
+
+  // event handler function for all cards
+  //  - on click, capture 'cardname' attr & check history array via .some()
+  //    - if no previous hits, randomize card order & increment score
+  //    - if card has been selected already:
+  //      - compare score to high score. replace if higher.
+  //      - reset current score to zero
+  //      - reset history array
+  //  - restart game loop: randomize cards, random order
+
+  // utility function:
+  //  randomize array order: override allCards array in state
+
+  // components
+  // app
+  //   header:       page title / github link
+  //   game:         primary component - state, handlers, logic
+  //     scoreboard:   current / high score
+  //     gameboard:    grid container for all cards
+  //       card:       generate individual cards
+
+  // introduction page
+  //  explain game
+  //  start button
+}
