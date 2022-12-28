1f98103b77227ff10dad53feaa2dcf9bf5e79486
Bryan Miller <bmilcs@yahoo.com>
2022-12-28 15:02:20 -0500

    Add cardList module: array of cardnames/svg components

---
+++ src/modules/cardList.js
@@ -0,0 +1,58 @@
+export const cardList = [
+  {
+    name: "Babel",
+    component: "Babel.js",
+  },
+  {
+    name: "CSS",
+    component: "CSS.js",
+  },
+  {
+    name: "ESLint",
+    component: "ESLint.js",
+  },
+  {
+    name: "Github",
+    component: "Github.js",
+  },
+  {
+    name: "HTML",
+    component: "HTML.js",
+  },
+  {
+    name: "Jest",
+    component: "Jest.js",
+  },
+  {
+    name: "JavaScript",
+    component: "JavaScript.js",
+  },
+  {
+    name: "Jest",
+    component: "Jest.js",
+  },
+  {
+    name: "Prettier",
+    component: "Prettier.js",
+  },
+  {
+    name: "React",
+    component: "React.js",
+  },
+  {
+    name: "SASS",
+    component: "Sass.js",
+  },
+  {
+    name: "VIM",
+    component: "VIM.js",
+  },
+  {
+    name: "VSCode",
+    component: "VSCode.js",
+  },
+  {
+    name: "Webpack",
+    component: "Webpack.js",
+  },
+];
