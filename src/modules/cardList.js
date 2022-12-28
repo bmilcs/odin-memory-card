df3dc1047ba1d28fefe4313473597193ec456293
Bryan Miller <bmilcs@yahoo.com>
2022-12-28 16:15:47 -0500

    Add additional svg components to cardList

--- src/modules/cardList.js
+++ src/modules/cardList.js
@@ -1,3 +1,4 @@
+import Angular from "../assets/Angular";
 import Babel from "../assets/Babel";
 import CSS from "../assets/CSS";
 import ESLint from "../assets/ESLint";
@@ -5,14 +6,22 @@ import Github from "../assets/Github";
 import HTML from "../assets/HTML";
 import JavaScript from "../assets/JavaScript";
 import Jest from "../assets/Jest";
+import NextJS from "../assets/NextJS";
+import NodeJS from "../assets/NodeJS";
 import Prettier from "../assets/Prettier";
 import React from "../assets/React";
 import Sass from "../assets/Sass";
+import TailwindCSS from "../assets/TailwindCSS";
 import VIM from "../assets/VIM";
 import VSCode from "../assets/VSCode";
+import Vue from "../assets/Vue";
 import Webpack from "../assets/Webpack";
 
 export const cardList = [
+  {
+    name: "Angular",
+    SVG: Angular,
+  },
   {
     name: "Babel",
     SVG: Babel,
@@ -42,6 +51,14 @@ export const cardList = [
     SVG: Jest,
   },
   {
+    name: "NextJS",
+    SVG: NextJS,
+  },
+  {
+    name: "NodeJS",
+    SVG: NodeJS,
+  },
+  {
     name: "Prettier",
     SVG: Prettier,
   },
@@ -54,8 +71,16 @@ export const cardList = [
     SVG: Sass,
   },
   {
+    name: "Tailwind",
+    SVG: TailwindCSS,
+  },
+  {
     name: "VIM",
     SVG: VIM,
+  },
+  {
+    name: "Vue",
+    SVG: Vue,
   },
   {
     name: "VSCode",
