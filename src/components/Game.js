1fefd10b00f2207b050bbbb837a8aae5aa46ef4c
Bryan Miller <bmilcs@yahoo.com>
2022-12-28 18:17:33 -0500

    Add basic game flow & score keeping functionality

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,19 +4,15 @@ import Gameboard from "./Gameboard";
 import Scoreboard from "./Scoreboard";
 
 export default function Game() {
-  const [cards, setCards] = useState(cardList);
-
-  // create a master array, containg all card objects
-  //  { name: "HTML", icon: HTMLSVG.js }
-  //  pass array as prop to cards component
-  //  render with dataset attribute 'cardname'
-  //  pass eventhandler function
-
   // state: initialize with:
   //  1. array 1: all cards, random order
   //  2. array 2: history of selected cards [empty]
   //  3. currentScore
   //  4. highScore
+  const [cards, setCards] = useState(cardList);
+  const [history, setHistory] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
 
   // event handler function for all cards
   //  - on click, capture 'cardname' attr & check history array via .some()
@@ -26,6 +22,47 @@ export default function Game() {
   //      - reset current score to zero
   //      - reset history array
   //  - restart game loop: randomize cards, random order
+
+  const handleCardClick = (e) => {
+    // get name of the card that was clicked: data-cardname attribute
+    const {
+      dataset: { cardname: newCard },
+    } = e.target;
+
+    if (isRepeatSelection(newCard)) roundOver();
+    else {
+      addToHistory(newCard);
+      incrementCurrentScore();
+    }
+  };
+
+  const isRepeatSelection = (currentCard) => {
+    return history.some((prevCard) => prevCard === currentCard);
+  };
+
+  const addToHistory = (cardName) => setHistory([...history, cardName]);
+
+  const incrementCurrentScore = () => setCurrentScore(currentScore + 1);
+
+  const roundOver = () => {
+    if (isNewHighScore()) updateHighScore();
+    resetCurrentScore();
+    clearHistory();
+  };
+
+  const updateHighScore = () => setHighScore(currentScore);
+
+  const isNewHighScore = () => currentScore > highScore;
+
+  const resetCurrentScore = () => setCurrentScore(0);
+
+  const clearHistory = () => setHistory([]);
+
+  // create a master array, containg all card objects
+  //  { name: "HTML", icon: HTMLSVG.js }
+  //  pass array as prop to cards component
+  //  render with dataset attribute 'cardname'
+  //  pass eventhandler function
 
   // utility function:
   //  randomize array order: override allCards array in state
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,7 +1,8 @@
 import "../styles/Scoreboard.scss";
 
-export default function Scoreboard() {
+export default function Scoreboard(props) {
   // render a Scoreboard containing current & high score
+  const { highScore, currentScore } = props;
 
   return (
     <div className="scoreboard-container">
@@ -9,11 +10,11 @@ export default function Scoreboard() {
       <div className="scoreboard-wrapper">
         <div className="score-container">
           <p className="scoretype-label">Current</p>
-          <h2 className="score current">8</h2>
+          <h2 className="score current">{currentScore}</h2>
         </div>
         <div className="score-container">
           <p className="scoretype-label">High</p>
-          <h2 className="score high">5</h2>
+          <h2 className="score high">{highScore}</h2>
         </div>
       </div>
     </div>
