ff66ba57041c34f26cceacc4fa38a5154d549eeb
Bryan Miller <bmilcs@yahoo.com>
2022-12-29 12:17:20 -0500

    Style responsive layout for smaller screens

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -6,15 +6,15 @@ export default function Scoreboard(props) {
 
   return (
     <div className="scoreboard-container">
-      <h3 className="scoreboard-title">Scores</h3>
+      {/* <h3 className="scoreboard-title">Scores</h3> */}
       <div className="scoreboard-wrapper">
         <div className="score-container">
-          <p className="scoretype-label">Current</p>
           <h2 className="score current">{currentScore}</h2>
+          <p className="scoretype-label">Current</p>
         </div>
         <div className="score-container">
-          <p className="scoretype-label">High</p>
           <h2 className="score high">{highScore}</h2>
+          <p className="scoretype-label">High</p>
         </div>
       </div>
     </div>
