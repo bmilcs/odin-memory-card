c79a7ac5f20c15511fb0b5d3a40e19812168c035
Bryan Miller <bmilcs@yahoo.com>
2022-12-29 13:03:21 -0500

    Refactor shuffle to trigger on useEffect & clean up code

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,14 +10,13 @@ export default function Game(props) {
   const handleCardClick = (e) => {
     // get name of card that was clicked via 'data-cardname' attribute
     const {
-      dataset: { cardname: newCard },
+      dataset: { cardname: clickedCard },
     } = e.target;
 
-    if (isRepeatSelection(newCard)) roundOver();
+    if (isRepeatSelection(clickedCard)) roundOver();
     else {
-      addToHistory(newCard);
+      addCardToHistory(clickedCard);
       incrementCurrentScore();
-      shuffleCards();
     }
   };
 
@@ -25,39 +24,9 @@ export default function Game(props) {
     return history.some((prevCard) => prevCard === currentCard);
   };
 
-  const addToHistory = (cardName) => setHistory([...history, cardName]);
+  const addCardToHistory = (cardName) => setHistory([...history, cardName]);
 
   const incrementCurrentScore = () => setCurrentScore(currentScore + 1);
-
-  const shuffleCards = () => {
-    // utilizes the fisher-yates (aka knuth) shuffle algorithm
-
-    // create a copy of the cards array
-    let shuffledDeck = [...cards];
-
-    // starting at the last card
-    let currentIndex = shuffledDeck.length;
-    let randomIndex;
-
-    // while cards remain that need to be shuffled
-    while (currentIndex !== 0) {
-      // randomly select another card: from index 0 to currentIndex - 1
-      // example: currentIndex of 3 = 0, 1, or 2
-      randomIndex = Math.floor(Math.random() * currentIndex);
-
-      // decrement current card
-      currentIndex--;
-
-      // swap current card with the random card via array destructuring
-      [shuffledDeck[currentIndex], shuffledDeck[randomIndex]] = [
-        shuffledDeck[randomIndex],
-        shuffledDeck[currentIndex],
-      ];
-    }
-
-    // update state with the newly shuffled deck array
-    setCards(shuffledDeck);
-  };
 
   const roundOver = () => {
     if (isNewHighScore()) updateHighScore();
@@ -65,13 +34,47 @@ export default function Game(props) {
     clearHistory();
   };
 
-  const updateHighScore = () => setHighScore(currentScore);
-
   const isNewHighScore = () => currentScore > highScore;
+
+  const updateHighScore = () => setHighScore(currentScore);
 
   const resetCurrentScore = () => setCurrentScore(0);
 
   const clearHistory = () => setHistory([]);
+
+  useEffect(() => {
+    const shuffleDeck = () => {
+      // utilizes the fisher-yates (aka knuth) shuffle algorithm
+      // create a copy of the cards array
+      let shuffledDeck = [...cards];
+
+      // starting at the last card
+      let currentIndex = shuffledDeck.length;
+      let randomIndex;
+
+      // while cards remain that need to be shuffled
+      while (currentIndex !== 0) {
+        // randomly select another card: from index 0 to currentIndex - 1
+        // example: currentIndex of 3 = 0, 1, or 2
+        randomIndex = Math.floor(Math.random() * currentIndex);
+
+        // decrement current card
+        currentIndex--;
+
+        // swap current card with the random card via array destructuring
+        [shuffledDeck[currentIndex], shuffledDeck[randomIndex]] = [
+          shuffledDeck[randomIndex],
+          shuffledDeck[currentIndex],
+        ];
+      }
+
+      // update state with the newly shuffled deck array
+      setCards(shuffledDeck);
+    };
+
+    // shuffle deck on currentScore change
+    shuffleDeck();
+  }, [currentScore]);
 
   return <Gameboard allCards={cards} onClick={handleCardClick} />;
 }
